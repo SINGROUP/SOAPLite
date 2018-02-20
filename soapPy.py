0e12d9bd40d1078117d26b06cd85b8c509f8fe61
marchunter <marc.o.j.jaeger@gmail.com>
2018-02-20 21:25:28 +0200

    python wrapper for soap function complete, soapPy.py with testSoapPy.py works now. the output is a matrix (Number of different elements * soapfeatures) the wrapper needs further testing with corner cases

--- soapPy.py
+++ soapPy.py
@@ -27,81 +27,44 @@ def format_ase2clusgeo(obj):
         pos_lst.append(pos_onetype)
         n_atoms_per_type_lst.append(n_onetype)
 
-    typeNs = atomtype_lst
+    typeNs = n_atoms_per_type_lst
     Ntypes = len(n_atoms_per_type_lst)
-    Nsize = n_atoms_per_type_lst
+    atomtype_lst
     Apos = np.concatenate(pos_lst).ravel()
-    return Apos, typeNs, Ntypes, Nsize, totalAN
+    return Apos, typeNs, Ntypes, atomtype_lst, totalAN
 
 
 def soap(obj, Hpos, NradBas=4, Lmax=5):
-
     # get clusgeo internal format for c-code
-    Apos, typeNs, Ntypes, Nsize, totalAN = format_ase2clusgeo(obj)
+    Apos, typeNs, py_Ntypes, atomtype_lst, totalAN = format_ase2clusgeo(obj)
     # flatten Hpos array
     Hpos = np.array(Hpos)
-    Hsize = Hpos.shape[0]
+    py_Hsize = Hpos.shape[0]
     Hpos = Hpos.flatten()
 
     # convert int to c_int
     l = c_int(Lmax)
-    Hsize = c_int(Hsize)
-    Ntypes = c_int(Ntypes)
+    Hsize = c_int(py_Hsize)
+    Ntypes = c_int(py_Ntypes)
     totalAN = c_int(totalAN)
-
+    Nsize = c_int(NradBas)
     #convert int array to c_int array
     typeNs = (c_int * len(typeNs))(*typeNs)
-    Nsize = (c_int * len(Nsize))(*Nsize)
 
     print(l, Hsize, Ntypes, totalAN, typeNs, Nsize)
     # convert to c_double arrays
     #Apos
-    axyz = (c_double * len(Apos))(*Apos)
+    axyz = (c_double * len(Apos))(*Apos.tolist())
     #Hpos
-    hxyz = (c_double * len(Hpos))(*Hpos)
-    #   c = libsoap.soap(c, axyz,hxyz,totalAN, Ntypes, Nsize, l, Hsize)
-    #   soap(axyz,typeNs.data_as(c_double_p),totalAN, Ntypes, Nsize, l, Hsize))
+    hxyz = (c_double * len(Hpos))(*Hpos.tolist())
 
     ### START SOAP###
     libsoap = CDLL('./libsoapPy.so')
     libsoap.soap.argtypes = [POINTER (c_double),POINTER (c_double), POINTER (c_double), POINTER (c_int),c_int,c_int,c_int,c_int,c_int]
-
     libsoap.soap.restype = POINTER (c_double)
-
-    c = libsoap.soap(axyz,hxyz,typeNs,totalAN, Ntypes, Nsize, l, Hsize)
-    #numArrya = array(c)
+    # double* c, double* Apos,double* Hpos,int* typeNs,
+    # int totalAN,int Ntypes,int Nsize, int l, int Hsize);
+    c = (c_double*(NradBas*NradBas*(Lmax+1)*py_Ntypes*py_Hsize))()
+    c = libsoap.soap(c, axyz,hxyz,typeNs,totalAN, Ntypes, Nsize, l, Hsize)
     #   return c;
-    return np.ctypeslib.as_array(c, shape=(Hsize,NradBas*NradBas*(lmax+1)*Ntypes))
-
-
-### INPUT ###
-
-parser = argparse.ArgumentParser(description='Give xyzfile')
-parser.add_argument('arguments', metavar='args', type=str, nargs='+', help='xyzfile')
-args = parser.parse_args()
-print("Passed arguments:", args.arguments)
-if len(args.arguments) < 1:
-    print('Not enough arguments')
-    exit(1)
-if len(args.arguments) > 1:
-    print('Too many arguments')
-    exit(1)
-
-inpfile = args.arguments[0]
-rootname = inpfile.replace(".xyz", "")
-
-### PROCESS ###
-structure = ase.io.read(inpfile, index = -1)
-
-Apos, typeNs, Ntypes, Nsize, totalAN = format_ase2clusgeo(structure)
-print("Apos")
-print(Apos)
-print("typeNs")
-print(typeNs)
-print("Ntypes")
-print(Ntypes)
-print("Nsize")
-print(Nsize)
-
-Hpos = [[2.3, 4.1 ,3.5],[2.2,3.3,4.4]]
-soap(structure, Hpos)
+    return np.ctypeslib.as_array(c, shape=(py_Hsize,NradBas*NradBas*(Lmax+1)*py_Ntypes))
--- testSoapPy.py
+++ testSoapPy.py
@@ -1,8 +1,54 @@
 import soapPy
+import ase
+import numpy as np
 
+# example structure
+#atoms = ase.io.read("teststr.xyz")
 
+h2o = ase.Atoms('H2O', positions=[[0.0, 0.1, 0.2 ], [3.2,4.5,5.5],[5.0, 6.0, 7.0]])
 
-Apos = [[[0.0, 0.1, 0.2 ], [3.2,4.5,5.5]],[[5.0, 6.0, 7.0]]]
+#Apos = [[[0.0, 0.1, 0.2 ], [3.2,4.5,5.5]],[[5.0, 6.0, 7.0]]]
 Hpos = [[2.3, 4.1 ,3.5],[2.2,3.3,4.4]]
-x = soapPy.soap(Apos, Hpos, 4, 5)
 
+x = soapPy.soap(h2o, Hpos, 4, 5)
+
+print("SOAP solution: ")
+print(x)
+print("soap size: ", np.shape(x))
+print("type: ", type(x))
+
+
+"""
+# old input from soapPy.py, in case it is still needed for debugging
+### INPUT ###
+
+parser = argparse.ArgumentParser(description='Give xyzfile')
+parser.add_argument('arguments', metavar='args', type=str, nargs='+', help='xyzfile')
+args = parser.parse_args()
+print("Passed arguments:", args.arguments)
+if len(args.arguments) < 1:
+    print('Not enough arguments')
+    exit(1)
+if len(args.arguments) > 1:
+    print('Too many arguments')
+    exit(1)
+
+inpfile = args.arguments[0]
+rootname = inpfile.replace(".xyz", "")
+
+### PROCESS ###
+structure = ase.io.read(inpfile, index = -1)
+
+Apos, typeNs, Ntypes, Nsize, totalAN = format_ase2clusgeo(structure)
+print("Apos")
+print(Apos)
+print("typeNs")
+print(typeNs)
+print("Ntypes")
+print(Ntypes)
+print("Nsize")
+print(Nsize)
+
+Hpos = [[2.3, 4.1 ,3.5],[2.2,3.3,4.4]]
+soap(structure, Hpos)
+"""
