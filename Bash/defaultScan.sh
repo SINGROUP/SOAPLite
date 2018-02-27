49546f9ff02b18387cec709e2bdadaf851055b81
Aki78 <kmorooka78@gmail.com>
2018-02-27 11:51:10 +0200

    cleanup directories

--- Bash/defaultScan.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-read -e -p "Select a .xyz File: " FILE
-echo $FILE
-./scanH $FILE 150 0.5 2.8 1.2 2.2 > "$FILE"_150_05_28_12_22.Hscan 
-
-    echo "${FILE}_150_05_27_12_22.Hscan was produced!"
-
-
--- Bash/getXYZ.sh
+++
@@ -1,3 +0,0 @@
-a=$(wc -l < $1)
-
-for i in `seq 1 $a`; do b=$(sed -n "${i}p" $1); sed -n "${b}p" $2; done
