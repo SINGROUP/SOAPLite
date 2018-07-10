ae35d75f1f1277862847557b96459cc8eafa0610
Lauri Himanen <lauri.himanen@aalto.fi>
2018-07-10 15:34:35 +0300

    Fixing import paths.

--- soaplite/__init__.py
+++ soaplite/__init__.py
@@ -1,2 +1,2 @@
-from soaplite.soaplite import  get_soap_locals,get_soap_structure,get_periodic_soap_locals,get_periodic_soap_structure
-from soaplite.genBasis import getBasisFunc
+from soaplite import get_soap_locals, get_soap_structure, get_periodic_soap_locals, get_periodic_soap_structure
+from genBasis import getBasisFunc
