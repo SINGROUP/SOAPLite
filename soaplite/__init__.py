74f9173b20795ec47084882db2bfdfae02e51396
Aki78 <kmorooka78@gmail.com>
2018-08-27 15:13:38 +0300

    forgot to add thisone

--- soaplite/__init__.py
+++ soaplite/__init__.py
@@ -1,3 +1,3 @@
-from .soaplite import get_soap_locals, get_soap_structure, get_periodic_soap_locals, get_periodic_soap_structure
+from .soaplite import get_soap_locals, get_soap_structure, get_periodic_soap_locals, get_periodic_soap_structure, get_soap_locals_general
 from .genBasis import getBasisFunc
 from .getBasis import getGns
