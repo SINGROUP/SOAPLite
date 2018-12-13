038ce258dbcbb632dcb8798c222bb74010fc68b4
Aki78 <kmorooka78@gmail.com>
2018-12-13 19:14:39 +0200

    added setup buffer

---
+++ soaplite/__init__Buffer.py
@@ -0,0 +1,3 @@
+from .core import get_soap_locals, get_soap_structure, get_periodic_soap_locals, get_periodic_soap_structure, get_soap_locals_general  
+from .genBasis import getBasisFunc
+from .getBasis import getGns
