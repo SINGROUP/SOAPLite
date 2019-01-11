3096c1bb5203215dd115c1d820ffe7947f55bc01
Lauri Himanen <lauri.himanen@aalto.fi>
2019-01-11 14:28:50 +0200

    Updated version number.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     setup(
         name="soaplite",
         url="https://github.com/SINGROUP/SOAPLite",
-        version="0.14.7",
+        version="1.0.0",
         description=("fast lightweight smooth overlap atomic position (SOAP) calculator. see github.com/SINGROUP/SOAPLite for detailed documentations."), author="Eiaki V. Morooka", author_email="eiaki.morooka@aalto.fi",
         packages=find_packages(),
         install_requires=[
